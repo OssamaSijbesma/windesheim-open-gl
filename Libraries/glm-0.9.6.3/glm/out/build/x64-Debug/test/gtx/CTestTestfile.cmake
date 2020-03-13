625adc997c78715a7e379d1393295862e82a3ea8
Ossama Sybesma <ossama_sybesma@hotmail.com>
2020-03-13 22:24:09 +0100

    More lib stuff

---
+++ Libraries/glm-0.9.6.3/glm/out/build/x64-Debug/test/gtx/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Apollo/Desktop/CG - Lecture 5 - 0 - Skeleton start/Libraries/glm-0.9.6.3/glm/test/gtx
+# Build directory: C:/Users/Apollo/Desktop/CG - Lecture 5 - 0 - Skeleton start/Libraries/glm-0.9.6.3/glm/out/build/x64-Debug/test/gtx
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
