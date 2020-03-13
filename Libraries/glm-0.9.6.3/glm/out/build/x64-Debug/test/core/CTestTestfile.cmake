3531c5f31af6a786fdd111052340bd223ba5c67d
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-03-13 22:34:48 +0100

    Only have the necessary

--- Libraries/glm-0.9.6.3/glm/out/build/x64-Debug/test/core/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Apollo/Desktop/CG - Lecture 5 - 0 - Skeleton start/Libraries/glm-0.9.6.3/glm/test/core
-# Build directory: C:/Users/Apollo/Desktop/CG - Lecture 5 - 0 - Skeleton start/Libraries/glm-0.9.6.3/glm/out/build/x64-Debug/test/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
