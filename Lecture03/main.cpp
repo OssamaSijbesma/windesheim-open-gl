c66a23817e0b5719dbe698a4178d7562cce2c118
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-03-16 13:40:20 +0100

    Added lecture 3

---
+++ Lecture03/main.cpp
@@ -0,0 +1,227 @@
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "glsl.h"
+#include <glm\gtc\matrix_transform.hpp>
+
+using namespace std;
+
+
+//--------------------------------------------------------------------------------
+// Consts
+//--------------------------------------------------------------------------------
+
+const int WIDTH = 800, HEIGHT = 600;
+
+const char* fragshader_name = "fragmentshader.fsh";
+const char* vertexshader_name = "vertexshader.vsh";
+
+unsigned const int DELTA_TIME = 10;
+
+
+//--------------------------------------------------------------------------------
+// Variables
+//--------------------------------------------------------------------------------
+
+// ID's
+GLuint program_id;
+GLuint vao;
+
+GLuint uniform_mvp;
+glm::mat4 mvp;
+
+
+//--------------------------------------------------------------------------------
+// Mesh variables
+//--------------------------------------------------------------------------------
+
+const GLfloat vertices[] =
+{
+    0.5, -0.5, 0.0,
+    -0.5, -0.5, 0.0,
+    0.0, 0.5, 0.0
+};
+
+const GLfloat colors[] =
+{
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+
+//--------------------------------------------------------------------------------
+// Keyboard handling
+//--------------------------------------------------------------------------------
+
+void keyboardHandler(unsigned char key, int a, int b)
+{
+    if (key == 27)
+        glutExit();
+}
+
+
+//--------------------------------------------------------------------------------
+// Rendering
+//--------------------------------------------------------------------------------
+
+void Render()
+{
+    // Define background
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Attach to program_id
+    glUseProgram(program_id);
+
+    // Send vao
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(0);
+
+    // Swap buffers
+    glutSwapBuffers();
+}
+
+void InitMatrices() 
+{
+    glm::mat4 model = glm::mat4();
+    glm::mat4 view = glm::lookAt(
+        glm::vec3(1.0, 0.0, 2.0),
+        glm::vec3(0.0, 0.0, 0.0),
+        glm::vec3(0.0, 1.0, 0.0));
+    glm::mat4 projection = glm::perspective(
+        glm::radians(45.0f),
+        1.0f * WIDTH / HEIGHT, 0.1f,
+        20.0f);
+
+    mvp = projection * view * model;
+}
+
+//------------------------------------------------------------
+// void Render(int n)
+// Render method that is called by the timer function
+//------------------------------------------------------------
+
+void Render(int n)
+{
+    Render();
+    glutTimerFunc(DELTA_TIME, Render, 0);
+}
+
+
+//------------------------------------------------------------
+// void InitGlutGlew(int argc, char **argv)
+// Initializes Glut and Glew
+//------------------------------------------------------------
+
+void InitGlutGlew(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutCreateWindow("Hello OpenGL");
+    glutDisplayFunc(Render);
+    glutKeyboardFunc(keyboardHandler);
+    glutTimerFunc(DELTA_TIME, Render, 0);
+
+    glewInit();
+}
+
+
+//------------------------------------------------------------
+// void InitShaders()
+// Initializes the fragmentshader and vertexshader
+//------------------------------------------------------------
+
+void InitShaders()
+{
+    char* vertexshader = glsl::readFile(vertexshader_name);
+    GLuint vsh_id = glsl::makeVertexShader(vertexshader);
+
+    char* fragshader = glsl::readFile(fragshader_name);
+    GLuint fsh_id = glsl::makeFragmentShader(fragshader);
+
+    program_id = glsl::makeShaderProgram(vsh_id, fsh_id);
+}
+
+
+//------------------------------------------------------------
+// void InitBuffers()
+// Allocates and fills buffers
+//------------------------------------------------------------
+
+void InitBuffers()
+{
+    GLuint position_id;
+    GLuint color_id;
+    GLuint vbo_vertices;
+    GLuint vbo_colors;
+
+    // vbo for vertices
+    glGenBuffers(1, &vbo_vertices);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_vertices);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // vbo for colors
+    glGenBuffers(1, &vbo_colors);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_colors);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Get vertex attributes
+    position_id = glGetAttribLocation(program_id, "position");
+    color_id = glGetAttribLocation(program_id, "color");
+
+    // Allocate memory for vao
+    glGenVertexArrays(1, &vao);
+
+    // Bind to vao
+    glBindVertexArray(vao);
+
+    // Bind vertices to vao
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_vertices);
+    glVertexAttribPointer(position_id, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(position_id);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Bind colors to vao
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_colors);
+    glVertexAttribPointer(color_id, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(color_id);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Stop bind to vao
+    glBindVertexArray(0);
+
+    // make uniform vars
+    uniform_mvp = glGetUniformLocation(program_id, "mvp");
+
+    // Fill uniform vars
+    glUseProgram(program_id);
+    glUniformMatrix4fv(uniform_mvp, 1, GL_FALSE, glm::value_ptr(mvp));
+}
+
+
+int main(int argc, char** argv)
+{
+    InitGlutGlew(argc, argv);
+    InitShaders();
+    InitMatrices();
+    InitBuffers();
+
+    // Hide console window
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow(hWnd, SW_HIDE);
+
+    // Main loop
+    glutMainLoop();
+
+    return 0;
+}
